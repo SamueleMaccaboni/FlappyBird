7c2aab90ea85db0d58a0e4429527e1ce952a8457
gnomo_macnia <151729853+SamueleMaccaboni@users.noreply.github.com>
2024-05-27 17:23:40 +0200

    Update background.js

--- Flappy_Bird-main/Game/background.js
+++ Flappy_Bird-main/Game/background.js
@@ -4,6 +4,7 @@ class Background {
         this.rect = this.element.getBoundingClientRect();
     }
 
+    // serve per ottenere la distanza dal bordo inferiore dell'elemento al bordo superiore
     getBottom() {
         return this.rect.bottom;
     }
