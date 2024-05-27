b87c180edbda980733c4c36efa427ff91ef92f8f
gnomo_macnia <151729853+SamueleMaccaboni@users.noreply.github.com>
2024-05-27 17:25:40 +0200

    Update pipe.js

--- Flappy_Bird-main/Game/pipe.js
+++ Flappy_Bird-main/Game/pipe.js
@@ -1,22 +1,26 @@
 class Pipe {
+    // inizializza un nuovo tubo con la classe CSS, la posizione top e left
     constructor(className, top, left) {
-        this.element = document.createElement('div');
-        this.element.className = className;
-        this.element.style.top = top;
-        this.element.style.left = left;
-        this.element.increase_score = '1';
+        this.element = document.createElement('div'); // crea un nuovo elemento div per rappresentare il tubo
+        this.element.className = className; // applica lo stile CSS nell'elemento
+        this.element.style.top = top; // imposta la posizione verticale
+        this.element.style.left = left; // imposta la posizione orizzontale
+        this.element.increase_score = '1'; // aumenta il punteggio quando l'uccello passa il tubo
     }
 
+    // muove il tubo verso sinistra
     move(moveSpeed) {
-        this.element.style.left = this.element.offsetLeft - moveSpeed + 'px';
+        this.element.style.left = this.element.offsetLeft - moveSpeed + 'px'; // aggiorna la posizione orizzontale 
     }
 
     remove() {
-        this.element.remove();
+        this.element.remove(); // rimuove l'elemento div del tubo 
     }
 
+    // ottiene le collisioni del tubo
     getRect() {
-        return this.element.getBoundingClientRect();
+        return this.element.getBoundingClientRect(); /* restituisce un oggetto che rappresenta le dimensioni e la posizione di un elemento 
+                                                        rispetto alla finestra */
     }
 }
 
